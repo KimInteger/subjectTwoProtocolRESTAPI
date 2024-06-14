a0b94bcbbef54a96292c46928673c09bb355cfc3
KimInteger <kjs20151240@gmail.com>
2024-06-14 17:44:43 +0900

    headTemp 모듈

--- headTemp.js
+++ headTemp.js
@@ -6,7 +6,9 @@ const headTemp = function(title,css) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
-    <link rel="stylesheet" href=${css}>
+    <link rel="stylesheet" href="${css}">
   </head>
   `;
 }
+
+module.exports = headTemp;
