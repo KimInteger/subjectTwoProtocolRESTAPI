f3fd9630d0e32374a79cef989db79ca82fd5a933
KimInteger <kjs20151240@gmail.com>
2024-06-14 16:29:36 +0900

    기본 모듈 세팅

--- datamodule/insertInfo.js
+++ datamodule/insertInfo.js
@@ -1,1 +1,12 @@
-console.log('insertInfo.js');
+const sqlite3 = require('sqlite3').verbose();
+
+const XMLHttpRequest = require('xhr2');
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('get','https://jsonplaceholder.typicode.com/todos');
+
+xhr.send();
+
+
+
