33b1d67f2a10140b90cb02f578026c60ce7b000a
KimInteger <kjs20151240@gmail.com>
2024-06-14 16:47:15 +0900

    모듈 호출 및 서버를 포트에 연결 및 서버 작동 확인

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('app.js');
+const http = require('node:http');
+
+const fs = require('node:fs');
+
+const qs = require('node:querystring');
+
+const path = require('node:path');
+
+const sqlite3 = require('sqlite3').verbose();
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req,res)=>{
+  console.log('서버 오픈');
+});
+
+server.listen(PORT, (err)=>{
+  if(err){
+    throw new Error('에러가 발생하였수' + err);
+  } else {
+    console.log("서버가 열렸다도르");
+    console.log(`http://localhost:${PORT}`);
+  }
+});
