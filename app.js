1d88c50ca5d6827efd0187e349be67f4a326e31b
KimInteger <kjs20151240@gmail.com>
2024-06-15 19:05:59 +0900

    template와 연결된 js및 css파일 정상작동 확인

--- app.js
+++ app.js
@@ -10,9 +10,40 @@ const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 8080;
 
+const temp = require('./templatemodule/template');
+
 const server = http.createServer((req,res)=>{
-  console.log('서버 오픈');
+  if(req.url === '/') {
+    res.writeHead(200,{"Content-type":'text/html; charset=UTF-8'});
+    res.end(temp.basicTemp('test','/test.css','와 테스트','test.js'));
+  } else if (req.url === '/test.css') {
+    fs.readFile('./test.css',(err,data)=>{
+      if(err){
+        throw new Error('에러 발생');
+      } else {
+        res.writeHead(200,{"Content-Type":"text/css"});
+        res.end(data);
+      }
+    });
+    
+  } else if (req.url === '/test.js') {
+    fs.readFile('./test.js',(err,data)=>{
+      if(err){
+        throw new Error('에러 발생');
+      } else {
+        res.writeHead(200,{"Content-Type":"text/css charset=UTF-8"});
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/favicon.ico') {
+    return;
+  } else {
+    res.writeHead(404,{"Content-Type":"text/plain; charset=UTF-8"});
+    res.end('페이지를 찾을 수 없습니다,');
+  }
 });
+
+
 
 server.listen(PORT, (err)=>{
   if(err){
