900367c107c596a20f1bcdd921a39aa78832c4ed
KimInteger <kjs20151240@gmail.com>
2024-06-16 09:09:48 +0900

    post실행문 작성 오류발생 branch 이동

--- app.js
+++ app.js
@@ -13,33 +13,69 @@ const PORT = process.env.PORT || 8080;
 const temp = require('./templatemodule/template');
 
 const server = http.createServer((req,res)=>{
-  if(req.url === '/') {
-    res.writeHead(200,{"Content-type":'text/html; charset=UTF-8'});
-    res.end(temp.basicTemp('test','/test.css','와 테스트','test.js'));
-  } else if (req.url === '/test.css') {
-    fs.readFile('./test.css',(err,data)=>{
-      if(err){
-        throw new Error('에러 발생');
-      } else {
-        res.writeHead(200,{"Content-Type":"text/css"});
-        res.end(data);
+  console.log(req.url);
+  if(req.method === 'GET'){
+    if(req.url === '/') {
+      fs.readFile('./index.html',(err,data)=>{
+        if(err){
+          throw new Error('에러 발생');
+        } else {
+          res.writeHead(200,{"Content-Type":"text/html; charset=UTF-8"});
+          res.end(data);
+        }
+      });
+    } else if (req.url === '/test.css') {
+      fs.readFile('./test.css',(err,data)=>{
+        if(err){
+          throw new Error('에러 발생');
+        } else {
+          res.writeHead(200,{"Content-Type":"text/css"});
+          res.end(data);
+        }
+      });
+    } else if (req.url === '/test.js') {
+      fs.readFile('./test.js',(err,data)=>{
+        if(err){
+          throw new Error('에러 발생');
+        } else {
+          res.writeHead(200,{"Content-Type":"text/css charset=UTF-8"});
+          res.end(data);
+        }
+      });
+    } else if (req.url === '/favicon.ico') {
+      return;
+    } else {
+      res.writeHead(404,{"Content-Type":"text/plain; charset=UTF-8"});
+      res.end('페이지를 찾을 수 없습니다,');
+    }
+  } else if ( req.method === 'POST'){
+    if(req.url === '/readDB') {
+      let testA = [];
+      async function a() {
+        const db = new sqlite3.Database('./database/info.db');
+        db.all("SELECT * FROM info",(err,rows)=>{
+          if(err){
+            throw new Error('에러가 발생');
+          } else {
+            rows.forEach((row)=>{
+              testA.push(row);
+            });
+            console.log(JSON.stringify(testA,null,2));
+  
+          }
+        });
+        db.close();
       }
-    });
-    
-  } else if (req.url === '/test.js') {
-    fs.readFile('./test.js',(err,data)=>{
-      if(err){
-        throw new Error('에러 발생');
-      } else {
-        res.writeHead(200,{"Content-Type":"text/css charset=UTF-8"});
-        res.end(data);
-      }
-    });
-  } else if (req.url === '/favicon.ico') {
-    return;
+      a();
+      res.writeHead(200,{"Content-Type":'text/plain; charset=UTF-8'});
+      res.end(temp.basicTemp('테스트','/test.css',JSON.stringify(testA,null,2)),'/test.js');
+    } else {
+      res.writeHead(404,{"Content-Type":"text/plain; charset=UTF-8"});
+      res.end("페이지를 찾을 수 없습니다.");  
+    }
   } else {
     res.writeHead(404,{"Content-Type":"text/plain; charset=UTF-8"});
-    res.end('페이지를 찾을 수 없습니다,');
+    res.end("페이지를 찾을 수 없습니다.");
   }
 });
 
